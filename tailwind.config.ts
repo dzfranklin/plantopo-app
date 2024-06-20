7a868a6874664ce95ba4f7c3d625b5707da11174
Daniel Franklin <daniel@danielzfranklin.org>
2024-06-20 22:59:10 +0100

    Fix tailwind.config/ts: include ./features

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,8 @@ import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
