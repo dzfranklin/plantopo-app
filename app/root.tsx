54de3db8df92c94e3050a88cc41c58eeedb372b4
Daniel Franklin <daniel@danielzfranklin.org>
2025-10-12 19:59:54 +0100

    add new favicon

--- app/root.tsx
+++ app/root.tsx
@@ -28,6 +28,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
